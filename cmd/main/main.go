00c94b99f3065aea5794945fed4d3ffc9b6d6511
Arnab Nath <arnabnath123456789@gmail.com>
2024-03-09 02:03:05 +0530

    problem in main file fixed

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,18 +4,15 @@ import (
 	
 	"log"
 	"net/http"
-
 	"github.com/arnabnath01/Go-bookshop/pkg/routes"
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm/dialects/mysql"
+	// "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main(){
 	r:=mux.NewRouter()
-	RegisterBookstoreRoutes(r)	
+	routes.RegisterBookstoreRoutes(r)
 	http.Handle("/",r)
 	log.Fatal(http.ListenAndServe(":8080",nil))
-
-	
 
 }
